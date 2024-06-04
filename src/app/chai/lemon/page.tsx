268cdb1b14fccf97c7331fef9edc134c16bfd5e7
and-is <andispaudel@gmail.com>
2024-06-04 18:21:54 +0545

    made hero section

--- src/app/chai/lemon/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function LemonPage(){
-    return (
-        <h1 className="text-3xl text-center">Lemon Tea</h1>
-    );
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import HeroSection from "@/componentes/HeroSection";
+
 export default function Home() {
   return (
-    <h1 className="text-2xl text-center">Chai aur code</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+    </main>
   );
 }
