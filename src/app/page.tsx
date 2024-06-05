50d23f30708b5e43dcc7da6ef6d209554099d8c4
and-is <andispaudel@gmail.com>
2024-06-05 09:21:32 +0545

    created testimonials and features sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import HeroSection from "@/componentes/HeroSection";
+import FeaturedCourses from "@/componentes/FeaturedCourses";
+import WhyChooseUs from "@/componentes/WhyChooseUs";
+import MusicSchoolTestimonials from "@/componentes/TestimonialCards";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials />
     </main>
   );
 }
